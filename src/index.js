4723869d5fcaa7586628c500f9c96a70fabf6d1a
Alaicev <altarev123456@mail,ru>
2022-01-08 18:43:16 +0400

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = "";
+    let str = String(n)
+
+    for (let i = str.length-1; i >= 0; i--){
+        result+= str[i]
+    }
+    if (n < 0) {
+        result = result.slice(0, -1)
+    }
+    return result
 }
